80b315b1b597b4ac2eec9b3aeab65354825f7953
Hawkeye <125225357+Hawkeye1590@users.noreply.github.com>
2023-12-05 23:38:56 -0500

    Add files via upload

---
+++ 332nd Legion Aux Mod/mod.cpp
@@ -0,0 +1,6 @@
+name = "332nd Legion Aux Mod";
+picture = "Logo.paa";
+overview = "332nd Legion Aux Mod";
+action = "https://www.youtube.com/watch?v=VVggXZPCuYw";
+logo = "Logo.paa";
+logoOver = "Logo.paa";
