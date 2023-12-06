1a1a5454ee1af0e232f3a38cbfa396b243cfbaba
Hawkeye <125225357+Hawkeye1590@users.noreply.github.com>
2023-12-05 23:50:59 -0500

    Add files via upload

---
+++ 332nd Legion Aux Mod/addons/332nd_equipment/backpacks/config.cpp
@@ -0,0 +1,205 @@
+class CfgPatches
+{
+	class 332nd_Legion_Backpacks
+	{
+		author = "332nd_AUX_TEAM";
+		
+		units[]=
+		{
+			"332nd_clone_backpack",
+			"332nd_clone_engineer_backpack",
+			"332nd_clone_heavy_backpack",
+			"332nd_clone_medic_backpack",
+			"332nd_clone_RTO_backpack",
+			"332nd_clone_Mini_RTO_backpack",
+			"332nd_clone_bag",
+		};
+		
+		weapons[]=
+		{
+			"",
+		};	
+		
+		requiredAddons[]=
+		{
+			"SWLB_equipment",
+		};
+
+		authors[] =
+        {
+			"332nd_AUX_TEAM",
+			"Hawkeye",
+			"Magic"
+        };
+	};
+};
+
+class CfgVehicles
+{
+	class SWLB_clone_backpack;
+	class SWLB_clone_backpack_eod;
+	class SWLB_clone_backpack_heavy;
+	class SWLB_clone_backpack_medic;
+	class SWLB_clone_backpack_RTO;
+	class SWLB_clone_bag;
+	class JLTS_Clone_backpack_RTO;
+	
+	class 332nd_clone_backpack: SWLB_clone_backpack
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone Backpack";
+		maximumLoad=200;
+		hiddenSelections[]=
+		{
+			"camo1",
+			"cover",
+			"heavy",
+			"medic",
+			"RTO"
+		};
+		hiddenselectionstextures[]=
+		{
+			"332nd_equipment\backpacks\textures\332nd_clone_backpack_co.paa",
+			"\SWLB_equipment\backpacks\data\SWLB_clone_backpack_co.paa",
+			"",
+			""
+		};
+	};
+	class 332nd_clone_engineer_backpack: SWLB_clone_backpack_eod
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone Engineer Backpack";
+		maximumLoad=300;
+		hiddenSelections[]=
+		{
+			"camo1",
+			"cover",
+			"heavy",
+			"medic",
+			"RTO"
+		};
+		hiddenselectionstextures[]=
+		{
+			"332nd_equipment\backpacks\textures\332nd_clone_engineer_backpack_co.paa",
+			"\SWLB_equipment\backpacks\data\SWLB_clone_backpack_co.paa",
+			"\SWLB_equipment\backpacks\data\SWLB_clone_backpack_co.paa",
+			""
+		};
+	};
+	class 332nd_clone_heavy_backpack: SWLB_clone_backpack_heavy
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone Heavy Backpack";
+		maximumLoad=400;
+		hiddenSelections[]=
+		{
+			"camo1",
+			"cover",
+			"heavy",
+			"medic",
+			"RTO"
+		};
+		hiddenselectionstextures[]=
+		{
+			"332nd_equipment\backpacks\textures\332nd_clone_heavy_backpack_co.paa",
+			"\SWLB_equipment\backpacks\data\SWLB_clone_backpack_co.paa",
+			"\SWLB_equipment\backpacks\data\SWLB_clone_backpack_co.paa",
+			"",
+			""
+		};
+	};
+	class 332nd_clone_medic_backpack: JLTS_Clone_backpack_RTO
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone Medic Backpack";
+		maximumLoad=400;
+		model="\SWLB_equipment\backpacks\swlb_clone_backpack.p3d";
+		hiddenSelections[]=
+		{
+			"camo1",
+			"cover",
+			"heavy",
+			"medic",
+			"RTO"
+		};
+		hiddenselectionstextures[]=
+		{
+			"332nd_equipment\backpacks\textures\332nd_clone_medic_backpack_co.paa",
+			"\SWLB_equipment\backpacks\data\SWLB_clone_backpack_co.paa",
+			"",
+			"\SWLB_equipment\backpacks\data\SWLB_clone_backpack_co.paa"
+		};
+	};
+	class 332nd_clone_bag: SWLB_clone_bag
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone Belt Bag";
+		maximumLoad=200;
+		hiddenselectionstextures[]=
+		{
+			"\SWLB_equipment\backpacks\data\bag_co.paa",
+			""
+		};
+	};
+	class 332nd_clone_RTO_backpack: JLTS_Clone_backpack_RTO
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone RTO Backpack";
+		maximumLoad=300;
+		model="\SWLB_equipment\backpacks\swlb_clone_backpack.p3d";
+		hiddenSelections[]=
+		{
+			"camo1",
+			"cover",
+			"heavy",
+			"medic",
+			"RTO"
+		};
+		hiddenselectionstextures[]=
+		{
+			"332nd_equipment\backpacks\textures\332nd_clone_RTO_backpack_co.paa",
+			"",
+			"",
+			"",
+			"\SWLB_equipment\backpacks\data\SWLB_clone_backpack_co.paa"
+		};
+	};
+	class 332nd_clone_Mini_RTO_backpack: JLTS_Clone_backpack_RTO
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone RTO Minipack";
+		maximumLoad=300;
+		model="\SWLB_equipment\backpacks\SWLB_clone_RTO_mini_backpack.p3d";
+		hiddenSelections[]=
+		{
+			"main",
+			"accumulator"
+		};
+		hiddenselectionstextures[]=
+		{
+			"\SWLB_equipment\backpacks\data\SWLB_clone_RTO_mini_backpack_night_co.paa",
+			"\SWLB_equipment\backpacks\data\SWLB_clone_RTO_mini_backpack_accumulator_night_co.paa"
+		};
+	};
+
+};
+
+class cfgMods
+{
+	author="332nd_AUX_TEAM";
+	timepacked="1687930196";
+};
---
+++ 332nd Legion Aux Mod/addons/332nd_equipment/jetpacks/config.cpp
@@ -0,0 +1,363 @@
+class CfgPatches
+{
+	class 332nd_Legion_JT12
+	{
+		author = "332nd_AUX_TEAM";
+		
+		units[]=
+		{
+			"332nd_clone_jt12",
+			"332nd_clone_RTO_jt12",		
+			"332nd_clone_heavy_jt12",	
+			"332nd_clone_medic_jt12",
+			"332nd_clone_crew_chief_jt12",
+			"332nd_clone_crew_chief_RTO_jt12",
+		};
+		
+		weapons[]=
+		{
+			"",
+		};	
+		
+		requiredAddons[]=
+		{
+			"442_equipment",
+			"kobra_core",
+		};
+
+		authors[] =
+        {
+			"332nd_AUX_TEAM",
+			"Hawkeye",
+        };
+	};
+};
+
+//Credits to the Kobra mod team for the original basework of the JT12s. Thank you for making it publicly available for adjustments and repacks. 
+
+class CfgVehicles
+{
+	class 442_jt12;
+	class 442_jt12_pj;
+	class 442_jt12_pj_radio;
+	class 442_jt12_radio;
+	
+	class 332nd_clone_jt12: 442_jt12
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone JT12";
+		picture = "kobra\kobra_core\kobra.paa";
+		model = "kobra\442_equipment\backpack\model\clone\k_jt12.p3d";
+		maximumLoad=200;
+		hiddenselections[] = 
+		{
+			"camo1"
+		};
+		hiddenselectionstextures[] = 
+		{
+			"332nd_equipment\jetpacks\textures\332nd_clone_jt12_co.paa"
+		};
+		NSM_jumppack_is_jumppack=1;
+		NSM_jumppack_spam_delay=1;
+		NSM_jumppack_energy_capacity=200;
+		NSM_jumppack_recharge=5;
+		NSM_jumppack_jump_effect_script="NSM_jumppack_effect_fnc_jt_21";
+		NSM_jumppack_effect_points[] = 
+		{
+			{
+				"effect1",
+				{0,.6,1} // {side to side , forward, verticle}
+			},
+			{
+				"effect2",
+				{0,.6,1}
+			}
+		};
+		NSM_jumppack_sound_ignite[]=
+		{
+			"NSM_Main\sounds\cdv21Start.ogg"
+		};
+		NSM_jumppack_sound_land[]=
+		{
+			"NSM_Main\sounds\cdv21End.ogg"
+		};
+		NSM_jumppack_sound_idle[]=
+		{
+			"NSM_Main\sounds\cdv21Idle.ogg"
+		};
+		NSM_jumppack_jump_types[]=
+		{	
+			{
+				"Short Jump",
+				{25,7,50,0,1,1}
+			}
+		};
+	};
+	class 332nd_clone_RTO_jt12: 442_jt12_radio
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone RTO JT12";
+		picture = "kobra\kobra_core\kobra.paa";
+		model = "kobra\442_equipment\backpack\model\clone\k_jt12_radio.p3d";
+		maximumLoad=300;
+		hiddenselectionstextures[] = 
+		{
+			"332nd_equipment\jetpacks\textures\332nd_clone_jt12_co.paa",
+			"332nd_equipment\jetpacks\textures\332nd_clone_jt12_radio_co.paa"
+		};
+		NSM_jumppack_is_jumppack=1;
+		NSM_jumppack_spam_delay=1;
+		NSM_jumppack_energy_capacity=200;
+		NSM_jumppack_recharge=5;
+		NSM_jumppack_jump_effect_script="NSM_jumppack_effect_fnc_jt_21";
+		NSM_jumppack_effect_points[] = 
+		{
+			{
+				"effect1",
+				{0,.6,1} // {side to side , forward, verticle}
+			},
+			{
+				"effect2",
+				{0,.6,1}
+			}
+		};
+		NSM_jumppack_sound_ignite[]=
+		{
+			"NSM_Main\sounds\cdv21Start.ogg"
+		};
+		NSM_jumppack_sound_land[]=
+		{
+			"NSM_Main\sounds\cdv21End.ogg"
+		};
+		NSM_jumppack_sound_idle[]=
+		{
+			"NSM_Main\sounds\cdv21Idle.ogg"
+		};
+		NSM_jumppack_jump_types[]=
+		{	
+			{
+				"Short Jump",
+				{25,7,50,0,1,1}
+			}
+		};
+	};
+	class 332nd_clone_heavy_jt12: 442_jt12
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone Heavy JT12";
+		picture = "kobra\kobra_core\kobra.paa";
+		model = "kobra\442_equipment\backpack\model\clone\k_jt12.p3d";
+		maximumLoad=400;
+		hiddenselections[] = 
+		{
+			"camo1"
+		};
+		hiddenselectionstextures[] = 
+		{
+			"332nd_equipment\jetpacks\textures\332nd_clone_jt12_co.paa"
+		};
+		NSM_jumppack_is_jumppack=1;
+		NSM_jumppack_spam_delay=1;
+		NSM_jumppack_energy_capacity=200;
+		NSM_jumppack_recharge=5;
+		NSM_jumppack_jump_effect_script="NSM_jumppack_effect_fnc_jt_21";
+		NSM_jumppack_effect_points[] = 
+		{
+			{
+				"effect1",
+				{0,.6,1} // {side to side , forward, verticle}
+			},
+			{
+				"effect2",
+				{0,.6,1}
+			}
+		};
+		NSM_jumppack_sound_ignite[]=
+		{
+			"NSM_Main\sounds\cdv21Start.ogg"
+		};
+		NSM_jumppack_sound_land[]=
+		{
+			"NSM_Main\sounds\cdv21End.ogg"
+		};
+		NSM_jumppack_sound_idle[]=
+		{
+			"NSM_Main\sounds\cdv21Idle.ogg"
+		};
+		NSM_jumppack_jump_types[]=
+		{	
+			{
+				"Short Jump",
+				{25,7,50,0,1,1}
+			}
+		};
+	};
+	class 332nd_clone_medic_jt12: 442_jt12
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone Medic JT12";
+		picture = "kobra\kobra_core\kobra.paa";
+		model = "kobra\442_equipment\backpack\model\clone\k_jt12.p3d";
+		maximumLoad=400;
+		hiddenselections[] = 
+		{
+			"camo1"
+		};
+		hiddenselectionstextures[] = 
+		{
+			"332nd_equipment\jetpacks\textures\332nd_clone_jt12_co.paa"
+		};
+		NSM_jumppack_is_jumppack=1;
+		NSM_jumppack_spam_delay=1;
+		NSM_jumppack_energy_capacity=200;
+		NSM_jumppack_recharge=5;
+		NSM_jumppack_jump_effect_script="NSM_jumppack_effect_fnc_jt_21";
+		NSM_jumppack_effect_points[] = 
+		{
+			{
+				"effect1",
+				{0,.6,1} // {side to side , forward, verticle}
+			},
+			{
+				"effect2",
+				{0,.6,1}
+			}
+		};
+		NSM_jumppack_sound_ignite[]=
+		{
+			"NSM_Main\sounds\cdv21Start.ogg"
+		};
+		NSM_jumppack_sound_land[]=
+		{
+			"NSM_Main\sounds\cdv21End.ogg"
+		};
+		NSM_jumppack_sound_idle[]=
+		{
+			"NSM_Main\sounds\cdv21Idle.ogg"
+		};
+		NSM_jumppack_jump_types[]=
+		{	
+			{
+				"Short Jump",
+				{25,7,50,0,1,1}
+			}
+		};
+	};
+	class 332nd_clone_crew_chief_jt12: 442_jt12_pj
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone Crew Chief JT12";
+		picture = "kobra\kobra_core\kobra.paa";
+		model = "kobra\442_equipment\backpack\model\clone\k_jt12.p3d";
+		maximumLoad=800;
+		hiddenselections[] = 
+		{
+			"camo1"
+		};
+		hiddenselectionstextures[] = 
+		{
+			"332nd_equipment\jetpacks\textures\332nd_clone_jt12_co.paa"
+		};
+		NSM_jumppack_is_jumppack=1;
+		NSM_jumppack_spam_delay=1;
+		NSM_jumppack_energy_capacity=200;
+		NSM_jumppack_recharge=10;
+		NSM_jumppack_jump_effect_script="NSM_jumppack_effect_fnc_jt_21";
+		NSM_jumppack_effect_points[] = 
+		{
+			{
+				"effect1",
+				{0,.6,1} // {side to side , forward, verticle}
+			},
+			{
+				"effect2",
+				{0,.6,1}
+			}
+		};
+		NSM_jumppack_sound_ignite[]=
+		{
+			"NSM_Main\sounds\cdv21Start.ogg"
+		};
+		NSM_jumppack_sound_land[]=
+		{
+			"NSM_Main\sounds\cdv21End.ogg"
+		};
+		NSM_jumppack_sound_idle[]=
+		{
+			"NSM_Main\sounds\cdv21Idle.ogg"
+		};
+		NSM_jumppack_jump_types[]=
+		{	
+			{
+				"Short Jump",
+				{25,7,50,0,1,1}
+			}
+		};
+	};
+	class 332nd_clone_crew_chief_RTO_jt12: 442_jt12_pj
+	{
+		author="Hawkeye";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd] Clone Crew Chief LR JT12";
+		picture = "kobra\kobra_core\kobra.paa";
+		model = "kobra\442_equipment\backpack\model\clone\k_jt12_radio.p3d";
+		maximumLoad=800;
+		hiddenselectionstextures[] = 
+		{
+			"332nd_equipment\jetpacks\textures\332nd_clone_jt12_co.paa",
+			"332nd_equipment\jetpacks\textures\332nd_clone_jt12_radio_co.paa"
+		};
+		NSM_jumppack_is_jumppack=1;
+		NSM_jumppack_spam_delay=1;
+		NSM_jumppack_energy_capacity=200;
+		NSM_jumppack_recharge=10;
+		NSM_jumppack_jump_effect_script="NSM_jumppack_effect_fnc_jt_21";
+		NSM_jumppack_effect_points[] = 
+		{
+			{
+				"effect1",
+				{0,.6,1} // {side to side , forward, verticle}
+			},
+			{
+				"effect2",
+				{0,.6,1}
+			}
+		};
+		NSM_jumppack_sound_ignite[]=
+		{
+			"NSM_Main\sounds\cdv21Start.ogg"
+		};
+		NSM_jumppack_sound_land[]=
+		{
+			"NSM_Main\sounds\cdv21End.ogg"
+		};
+		NSM_jumppack_sound_idle[]=
+		{
+			"NSM_Main\sounds\cdv21Idle.ogg"
+		};
+		NSM_jumppack_jump_types[]=
+		{	
+			{
+				"Short Jump",
+				{25,7,50,0,1,1}
+			}
+		};
+	};
+};
+
+class cfgMods
+{
+	author="332nd_AUX_TEAM";
+	timepacked="1687930196";
+};
