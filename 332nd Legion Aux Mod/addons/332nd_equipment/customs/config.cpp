713cde8ae877d7ce4a6f53040c59fd19233083c9
MagicalLoaf <loaf2003dsc@gmail.com>
2024-01-12 02:37:37 +0100

    Customs

--- 332nd Legion Aux Mod/addons/332nd_equipment/customs/config.cpp
+++ 332nd Legion Aux Mod/addons/332nd_equipment/customs/config.cpp
@@ -7,6 +7,8 @@ class CfgPatches
 		units[]=
 		{
 			"332nd_Riddle_Custom",
+			"332nd_Uncle_Custom",
+			"332nd_Magika_Custom",
 		};
 		
 		weapons[]=
@@ -24,21 +26,16 @@ class CfgPatches
 			"332nd_Mixer_Custom_P2_helmet",
 			"332nd_Kirito_Custom_P2_helmet",
 			"332nd_Mini_Custom_P2_helmet",
+			"332nd_Brotha_Custom_P2_helmet",
+			"332nd_Sapphire_Custom_P2_helmet",
+			"332nd_Beast_Custom_P2_helmet",
 			"332nd_Ginger_Custom_P2_Pilot_helmet",
 			"332nd_Shadow_Custom_P2_Pilot_helmet",
 			"332nd_Misfit_Custom_P2_SpecOps_helmet",
+			"332nd_Undead_Custom_P2_SpecOps_helmet",
 			"332nd_Riddle_Custom_uniform",
-			"332nd_CR_uniform",
-			"332nd_CRC_uniform",
-			"332nd_GI_CT_uniform",
-			"332nd_GI_CST_uniform",
-			"332nd_GI_CVT_uniform",
-			"332nd_GI_CSP_uniform",
-			"332nd_GI_CP_uniform",
-			"332nd_GI_CS_uniform",
-			"332nd_GI_CSM_uniform",
-			"332nd_GI_CL_uniform",
-			"332nd_GI_CC_uniform",
+			"332nd_Uncle_Custom_uniform",
+			"332nd_Magika_Custom_uniform",
 		};	
 		
 		requiredAddons[]=
@@ -447,6 +444,90 @@ class CfgWeapons
                 };
 			};
 	};
+	class 332nd_Brotha_Custom_P2_helmet: SWLB_clone_P2_helmet
+	{
+		author="Magika";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd Custom] Brotha's Helmet (Phase 2)";
+		hiddenSelectionsTextures[]=
+		{
+			"\332nd_equipment\customs\textures\gi\332nd_Brotha_Helm.paa"
+		};
+		ace_hearing_protection=1;
+		ace_hearing_lowerVolume=0.40000001;
+		class HitpointsProtectionInfo
+            {
+                class Head
+                {
+                    hitPointName ="HitHead";
+                    armor=20;
+                    passThrough=0.5;
+                };
+                class Face
+                {
+                    hitPointName ="HitFace";
+                    armor=20;
+                    passThrough=0.30000001;
+                };
+			};
+	};
+	class 332nd_Sapphire_Custom_P2_helmet: SWLB_clone_P2_helmet
+	{
+		author="Magika";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd Custom] Sapphire's Helmet (Phase 2)";
+		hiddenSelectionsTextures[]=
+		{
+			"\332nd_equipment\customs\textures\gi\332nd_Sapphire_Helm.paa"
+		};
+		ace_hearing_protection=1;
+		ace_hearing_lowerVolume=0.40000001;
+		class HitpointsProtectionInfo
+            {
+                class Head
+                {
+                    hitPointName ="HitHead";
+                    armor=20;
+                    passThrough=0.5;
+                };
+                class Face
+                {
+                    hitPointName ="HitFace";
+                    armor=20;
+                    passThrough=0.30000001;
+                };
+			};
+	};
+	class 332nd_Beast_Custom_P2_helmet: SWLB_clone_P2_helmet
+	{
+		author="Magika";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd Custom] Beast's Helmet (Phase 2)";
+		hiddenSelectionsTextures[]=
+		{
+			"\332nd_equipment\customs\textures\gi\332nd_Beast_Helm.paa"
+		};
+		ace_hearing_protection=1;
+		ace_hearing_lowerVolume=0.40000001;
+		class HitpointsProtectionInfo
+            {
+                class Head
+                {
+                    hitPointName ="HitHead";
+                    armor=20;
+                    passThrough=0.5;
+                };
+                class Face
+                {
+                    hitPointName ="HitFace";
+                    armor=20;
+                    passThrough=0.30000001;
+                };
+			};
+	};
 
 	// PHASE 2 PILOT HELMS
 
@@ -539,6 +620,35 @@ class CfgWeapons
                 };
 			};
 	};
+	class 332nd_Undead_Custom_P2_SpecOps_helmet: SWLB_P2_SpecOps_Helmet
+	{
+		author="Magika";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd Custom] Undead's Helmet (Phase 2)";
+		picture="\SWLB_clones\data\ui\icon_SWLB_clone_helmet_p1_ca.paa";
+		hiddenSelectionsTextures[]=
+		{
+			"\332nd_equipment\customs\textures\gravedigger\332nd_Undead_Helm.paa"
+		};
+		ace_hearing_protection=1;
+		ace_hearing_lowerVolume=0.40000001;
+		class HitpointsProtectionInfo
+            {
+                class Head
+                {
+                    hitPointName ="HitHead";
+                    armor=28;
+                    passThrough=0.5;
+                };
+                class Face
+                {
+                    hitPointName ="HitFace";
+                    armor=28;
+                    passThrough=0.30000001;
+                };
+			};
+	};
 	//Uniforms
 	class 332nd_Riddle_Custom_uniform: SWLB_clone_uniform
 	{
@@ -550,6 +660,34 @@ class CfgWeapons
 		{
 			uniformModel="-";
 			uniformClass="332nd_Riddle_Custom";
+			containerClass="Supply150";
+			mass=40;
+		};
+	};
+	class 332nd_Uncle_Custom_uniform: SWLB_clone_uniform
+	{
+		author="Magika";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd Custom] Uncle's Uniform";
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="332nd_Uncle_Custom";
+			containerClass="Supply150";
+			mass=40;
+		};
+	};
+	class 332nd_Magika_Custom_uniform: SWLB_clone_uniform
+	{
+		author="Magika";
+		scopecurator=2;
+		scope=2;
+		displayName="[332nd Custom] Magika's Uniform";
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="332nd_Magika_Custom";
 			containerClass="Supply150";
 			mass=40;
 		};
@@ -589,6 +727,446 @@ class CfgVehicles
 		{
 			"SWLB_clone_basic_armor",
 			"332nd_Riddle_Custom_P2_helmet",
+			"ItemMap",
+			"ItemCompass",
+			"ItemWatch",
+			"SWLB_comlink"
+		};
+		weapons[]=
+		{
+			"SWLW_DC15A",
+			"Throw",
+			"Put"
+		};
+		respawnWeapons[]=
+		{
+			"SWLW_DC15A",
+			"Throw",
+			"Put"
+		};
+		backpack="";
+		armor=2;
+		armorStructural=4;
+		explosionShielding=0.40000001;
+		minTotalDamageThreshold=0.001;
+		impactDamageMultiplier=0.5;
+		class HitPoints: HitPoints
+		{
+			class HitFace
+			{
+				armor=1;
+				material=-1;
+				name="face_hub";
+				passThrough=0.80000001;
+				radius=0.079999998;
+				explosionShielding=0.1;
+				minimalHit=0.0099999998;
+			};
+			class HitNeck: HitFace
+			{
+				armor=1;
+				material=-1;
+				name="neck";
+				passThrough=0.80000001;
+				radius=0.1;
+				explosionShielding=0.5;
+				minimalHit=0.0099999998;
+			};
+			class HitHead: HitNeck
+			{
+				armor=1;
+				material=-1;
+				name="head";
+				passThrough=0.80000001;
+				radius=0.2;
+				explosionShielding=0.5;
+				minimalHit=0.0099999998;
+				depends="HitFace max HitNeck";
+			};
+			class HitPelvis: HitHead
+			{
+				armor=8;
+				material=-1;
+				name="pelvis";
+				passThrough=0.80000001;
+				radius=0.23999999;
+				explosionShielding=3;
+				visual="injury_body";
+				minimalHit=0.0099999998;
+				depends="";
+			};
+			class HitAbdomen: HitPelvis
+			{
+				armor=6;
+				material=-1;
+				name="spine1";
+				passThrough=0.80000001;
+				radius=0.16;
+				explosionShielding=3;
+				visual="injury_body";
+				minimalHit=0.0099999998;
+			};
+			class HitDiaphragm: HitAbdomen
+			{
+				armor=6;
+				material=-1;
+				name="spine2";
+				passThrough=0.33000001;
+				radius=0.18000001;
+				explosionShielding=6;
+				visual="injury_body";
+				minimalHit=0.0099999998;
+			};
+			class HitChest: HitDiaphragm
+			{
+				armor=8;
+				material=-1;
+				name="spine3";
+				passThrough=0.33000001;
+				radius=0.18000001;
+				explosionShielding=6;
+				visual="injury_body";
+				minimalHit=0.0099999998;
+			};
+			class HitBody: HitChest
+			{
+				armor=1000;
+				material=-1;
+				name="body";
+				passThrough=1;
+				radius=0;
+				explosionShielding=6;
+				visual="injury_body";
+				minimalHit=0.0099999998;
+				depends="HitPelvis max HitAbdomen max HitDiaphragm max HitChest";
+			};
+			class HitArms: HitBody
+			{
+				armor=6;
+				material=-1;
+				name="arms";
+				passThrough=1;
+				radius=0.1;
+				explosionShielding=3;
+				visual="injury_hands";
+				minimalHit=0.0099999998;
+				depends="0";
+			};
+			class HitHands: HitArms
+			{
+				armor=6;
+				material=-1;
+				name="hands";
+				passThrough=1;
+				radius=0.1;
+				explosionShielding=1;
+				visual="injury_hands";
+				minimalHit=0.0099999998;
+				depends="HitArms";
+			};
+			class HitLegs: HitHands
+			{
+				armor=6;
+				material=-1;
+				name="legs";
+				passThrough=1;
+				radius=0.14;
+				explosionShielding=3;
+				visual="injury_legs";
+				minimalHit=0.0099999998;
+				depends="0";
+			};
+			class Incapacitated: HitLegs
+			{
+				armor=1000;
+				material=-1;
+				name="body";
+				passThrough=1;
+				radius=0;
+				explosionShielding=3;
+				visual="";
+				minimalHit=0;
+				depends="(((Total - 0.25) max 0) + ((HitHead - 0.25) max 0) + ((HitBody - 0.25) max 0)) * 2";
+			};
+			class HitLeftArm
+			{
+				armor=6;
+				material=-1;
+				name="hand_l";
+				passThrough=1;
+				radius=0.079999998;
+				explosionShielding=3;
+				visual="injury_hands";
+				minimalHit=0.0099999998;
+			};
+			class HitRightArm: HitLeftArm
+			{
+				name="hand_r";
+			};
+			class HitLeftLeg
+			{
+				armor=6;
+				material=-1;
+				name="leg_l";
+				passThrough=1;
+				radius=0.1;
+				explosionShielding=3;
+				visual="injury_legs";
+				minimalHit=0.0099999998;
+			};
+			class HitRightLeg: HitLeftLeg
+			{
+				name="leg_r";
+			};
+		};
+    };
+	class 332nd_Uncle_Custom: SWLB_clone_base_P2
+	{
+		author="Magika";
+		scopecurator=2;
+		scope=2;
+		displayName="[Phase 2] Uncle";
+		uniformclass="332nd_Uncle_Custom_uniform";
+		faction="332nd_Legion";
+		editorSubcategory="332nd_Categ_Customs";
+		hiddenSelectionsTextures[]=
+		{
+			"\332nd_equipment\customs\textures\gi\332nd_Uncle_Upper.paa",
+			"\332nd_equipment\customs\textures\gi\332nd_Uncle_Lower.paa"
+		};
+		linkedItems[]=
+		{
+			"SWLB_clone_basic_armor",
+			"332nd_Uncle_Custom_P2_helmet",
+			"ItemMap",
+			"ItemCompass",
+			"ItemWatch",
+			"SWLB_comlink"
+		};
+		respawnLinkedItems[]=
+		{
+			"SWLB_clone_basic_armor",
+			"332nd_Uncle_Custom_P2_helmet",
+			"ItemMap",
+			"ItemCompass",
+			"ItemWatch",
+			"SWLB_comlink"
+		};
+		weapons[]=
+		{
+			"SWLW_DC15A",
+			"Throw",
+			"Put"
+		};
+		respawnWeapons[]=
+		{
+			"SWLW_DC15A",
+			"Throw",
+			"Put"
+		};
+		backpack="";
+		armor=2;
+		armorStructural=4;
+		explosionShielding=0.40000001;
+		minTotalDamageThreshold=0.001;
+		impactDamageMultiplier=0.5;
+		class HitPoints: HitPoints
+		{
+			class HitFace
+			{
+				armor=1;
+				material=-1;
+				name="face_hub";
+				passThrough=0.80000001;
+				radius=0.079999998;
+				explosionShielding=0.1;
+				minimalHit=0.0099999998;
+			};
+			class HitNeck: HitFace
+			{
+				armor=1;
+				material=-1;
+				name="neck";
+				passThrough=0.80000001;
+				radius=0.1;
+				explosionShielding=0.5;
+				minimalHit=0.0099999998;
+			};
+			class HitHead: HitNeck
+			{
+				armor=1;
+				material=-1;
+				name="head";
+				passThrough=0.80000001;
+				radius=0.2;
+				explosionShielding=0.5;
+				minimalHit=0.0099999998;
+				depends="HitFace max HitNeck";
+			};
+			class HitPelvis: HitHead
+			{
+				armor=8;
+				material=-1;
+				name="pelvis";
+				passThrough=0.80000001;
+				radius=0.23999999;
+				explosionShielding=3;
+				visual="injury_body";
+				minimalHit=0.0099999998;
+				depends="";
+			};
+			class HitAbdomen: HitPelvis
+			{
+				armor=6;
+				material=-1;
+				name="spine1";
+				passThrough=0.80000001;
+				radius=0.16;
+				explosionShielding=3;
+				visual="injury_body";
+				minimalHit=0.0099999998;
+			};
+			class HitDiaphragm: HitAbdomen
+			{
+				armor=6;
+				material=-1;
+				name="spine2";
+				passThrough=0.33000001;
+				radius=0.18000001;
+				explosionShielding=6;
+				visual="injury_body";
+				minimalHit=0.0099999998;
+			};
+			class HitChest: HitDiaphragm
+			{
+				armor=8;
+				material=-1;
+				name="spine3";
+				passThrough=0.33000001;
+				radius=0.18000001;
+				explosionShielding=6;
+				visual="injury_body";
+				minimalHit=0.0099999998;
+			};
+			class HitBody: HitChest
+			{
+				armor=1000;
+				material=-1;
+				name="body";
+				passThrough=1;
+				radius=0;
+				explosionShielding=6;
+				visual="injury_body";
+				minimalHit=0.0099999998;
+				depends="HitPelvis max HitAbdomen max HitDiaphragm max HitChest";
+			};
+			class HitArms: HitBody
+			{
+				armor=6;
+				material=-1;
+				name="arms";
+				passThrough=1;
+				radius=0.1;
+				explosionShielding=3;
+				visual="injury_hands";
+				minimalHit=0.0099999998;
+				depends="0";
+			};
+			class HitHands: HitArms
+			{
+				armor=6;
+				material=-1;
+				name="hands";
+				passThrough=1;
+				radius=0.1;
+				explosionShielding=1;
+				visual="injury_hands";
+				minimalHit=0.0099999998;
+				depends="HitArms";
+			};
+			class HitLegs: HitHands
+			{
+				armor=6;
+				material=-1;
+				name="legs";
+				passThrough=1;
+				radius=0.14;
+				explosionShielding=3;
+				visual="injury_legs";
+				minimalHit=0.0099999998;
+				depends="0";
+			};
+			class Incapacitated: HitLegs
+			{
+				armor=1000;
+				material=-1;
+				name="body";
+				passThrough=1;
+				radius=0;
+				explosionShielding=3;
+				visual="";
+				minimalHit=0;
+				depends="(((Total - 0.25) max 0) + ((HitHead - 0.25) max 0) + ((HitBody - 0.25) max 0)) * 2";
+			};
+			class HitLeftArm
+			{
+				armor=6;
+				material=-1;
+				name="hand_l";
+				passThrough=1;
+				radius=0.079999998;
+				explosionShielding=3;
+				visual="injury_hands";
+				minimalHit=0.0099999998;
+			};
+			class HitRightArm: HitLeftArm
+			{
+				name="hand_r";
+			};
+			class HitLeftLeg
+			{
+				armor=6;
+				material=-1;
+				name="leg_l";
+				passThrough=1;
+				radius=0.1;
+				explosionShielding=3;
+				visual="injury_legs";
+				minimalHit=0.0099999998;
+			};
+			class HitRightLeg: HitLeftLeg
+			{
+				name="leg_r";
+			};
+		};
+    };
+	class 332nd_Magika_Custom: SWLB_clone_base_P2
+	{
+		author="Magika";
+		scopecurator=2;
+		scope=2;
+		displayName="[Phase 2] Magika";
+		uniformclass="332nd_Magika_Custom_uniform";
+		faction="332nd_Legion";
+		editorSubcategory="332nd_Categ_Customs";
+		hiddenSelectionsTextures[]=
+		{
+			"\332nd_equipment\customs\textures\gi\332nd_Magika_Upper.paa",
+			"\332nd_equipment\customs\textures\gi\332nd_Magika_Lower.paa"
+		};
+		linkedItems[]=
+		{
+			"SWLB_clone_basic_armor",
+			"332nd_Magika_Custom_P2_helmet",
+			"ItemMap",
+			"ItemCompass",
+			"ItemWatch",
+			"SWLB_comlink"
+		};
+		respawnLinkedItems[]=
+		{
+			"SWLB_clone_basic_armor",
+			"332nd_Magika_Custom_P2_helmet",
 			"ItemMap",
 			"ItemCompass",
 			"ItemWatch",
